77d02ff0c26eb2ce9323190af4c8d84526afe364
patrickdeyoreo <pdeyoreo@gmail.com>
2019-11-04 23:43:31 -0800

    Spawning zombie processes... What a trip

--- 0x05-processes_and_signals/102-zombie.c
+++ 0x05-processes_and_signals/102-zombie.c
@@ -1,1 +1,32 @@
-#!/usr/bin/env bash
+#define _GNU_SOURCE
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#define STACKSIZE 8
+#define N_ZOMBIES 5
+
+int infinite_while(void)
+{
+	while (1)
+	{
+		sleep(1);
+	}
+	return (0);
+}
+
+int main(void)
+{
+	pid_t zpid = 0;
+	int zcount = 0;
+
+	while (zcount++ < N_ZOMBIES)
+	{
+		zpid = fork();
+		if (zpid)
+			printf("Zombie process created, PID: %d\n", zpid);
+		else
+			return (0);
+	}
+	return (infinite_while());
+}
