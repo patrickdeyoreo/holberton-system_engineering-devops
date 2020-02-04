0bb0c5fddd8a7a5e1b0a1761916337c6b12fdff5
Patrick DeYoreo <pdeyoreo@gmail.com>
2020-02-03 16:17:22 -0800

    Fixed collection of tasks for each user

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -2,27 +2,23 @@
 """
 Summarize an employee's TODO list progress
 """
-import argparse
-import os
-import requests
-import sys
+from argparse import ArgumentParser
+from os import path
+from requests import get
+from sys import argv
 
-PREFIX = 'https://jsonplaceholder.typicode.com'
-ROUTES = {
-    'users': lambda user_id: '/'.join([PREFIX, 'users', user_id]),
-    'todos': lambda user_id: '/'.join([PREFIX, 'users', user_id, 'todos'])
-}
+API = 'https://jsonplaceholder.typicode.com'
 
 if __name__ == '__main__':
 
-    parser = argparse.ArgumentParser(prog=os.path.basename(sys.argv[0]))
+    parser = ArgumentParser(prog=path.basename(argv[0]))
     parser.add_argument('id', type=int, help='employee ID')
     args = parser.parse_args()
-    user = requests.get(ROUTES['users'](str(args.id)), timeout=5).json()
-    todo = requests.get(ROUTES['todos'](str(args.id)), timeout=5).json()
-    completed = [task for task in todo if task.get('completed') is True]
-    print('Employee {name} is done with tasks({done}/{total}):'.format(
-        name=user.get('name'), done=len(completed), total=len(todo)
+    user = get('/'.join([API, 'users', str(args.id)])).json()
+    todo = get('/'.join([API, 'todos']), params={'userId': args.id}).json()
+    completed = [task for task in todo if task['completed'] is True]
+    print('Employee {} is done with tasks({}/{}):'.format(
+        user['name'], len(completed), len(todo)
     ))
     for task in completed:
         print('\t {}'.format(task['title']))
