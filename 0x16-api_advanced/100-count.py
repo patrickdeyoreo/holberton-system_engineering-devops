cea25c83d6194a53688de684b8573cdb49dac9ac
Patrick DeYoreo <29614644+patrickdeyoreo@users.noreply.github.com>
2020-02-06 13:30:24 -0800

    Reworked count_words() mmmto increment every match once

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -8,15 +8,15 @@ URL = 'https://www.reddit.com/r/{}/hot.json'
 USER_AGENT = 'Mozilla/5.0 (Linux x86_64; rv:72.0) Gecko/20100101 Firefox/72.0'
 
 
-def count_words(subreddit, word_list, **kwargs):
+def count_words(subreddit, words, **kwargs):
     """
     Query reddit for hot posts and print total occurrences of each keyword
     """
-    totals = kwargs.setdefault('totals', dict.fromkeys(word_list, 0))
+    totals = kwargs.setdefault('totals', dict.fromkeys(words, 0))
     params = {
         'after': kwargs.setdefault('after'),
         'count': kwargs.setdefault('count', 0),
-        'limit': kwargs.setdefault('limit', 100)
+        'limit': kwargs.setdefault('limit', 100),
     }
     r = requests.get(
         URL.format(subreddit),
@@ -28,13 +28,16 @@ def count_words(subreddit, word_list, **kwargs):
     if r.status_code == 200:
         results = r.json()['data']
         for post in results['children']:
-            for word in post['data']['title'].split():
-                if word.lower() in totals:
-                    totals[word.lower()] += 1
+            words = post['data']['title'].split()
+            words = {w.casefold() for w in words}
+            for key in totals:
+                if key.casefold() in words:
+                    totals[key] += 1
         if results['after'] is not None:
             kwargs['after'] = results['after']
             kwargs['count'] += kwargs['limit']
-            return count_words(subreddit, word_list, **kwargs)
-        word_list = filter(totals.get, totals)
-        for word in sorted(word_list, key=totals.get, reverse=True):
-            print('{}: {}'.format(word, totals[word]))
+            return count_words(subreddit, totals, **kwargs)
+        keys = filter(totals.get, totals)
+        keys.sort(key=totals.get, reverse=True)
+        for key in keys:
+            print('{}: {}'.format(key, totals[key]))
