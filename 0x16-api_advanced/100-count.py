0a0e16718542516270fb2d047c40dadef659052a
Patrick DeYoreo <pdeyoreo@gmail.com>
2020-02-07 01:14:25 -0800

    Rewrote 100-count.py

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -6,36 +6,33 @@ import re
 import requests
 
 URL = 'https://www.reddit.com/r/{}/hot.json'
-USER_AGENT = 'Mozilla/5.0 (Linux x86_64; rv:72.0) Gecko/20100101 Firefox/72.0'
 
 
-def count_words(subreddit, word_list, **kwargs):
+def count_words(subreddit, words, totals=None, after=None):
     """
     Query reddit for hot posts and print total occurrences of each keyword
     """
-    totals = kwargs.setdefault('totals', dict.fromkeys(word_list, 0))
-    params = {
-        'after': kwargs.setdefault('after'),
-        'limit': kwargs.setdefault('limit', 100),
-    }
     r = requests.get(
         URL.format(subreddit),
-        headers={'User-Agent': USER_AGENT},
-        params=params,
+        headers={'User-Agent': 'Mozilla/5.0'},
+        params={'after': after, 'limit': 100},
         allow_redirects=False,
-        timeout=30,
     )
     if r.status_code == 200:
-        results = r.json()['data']
-        titles = [post['data']['title'] for post in results['children']]
+        totals = totals or dict.fromkeys(words, 0)
+        result = r.json().get('data', {})
+        titles = [
+            hotpost.get('data', {}).get('title', '') for
+            hotpost in result.get('children', [])
+        ]
         for key in totals:
             for title in titles:
                 totals[key] += len(
                     re.findall(r'(?:^| )' + key + '(?:$| )', title, re.I)
                 )
-        if results['after'] is not None:
-            kwargs['after'] = results['after']
-            return count_words(subreddit, [], **kwargs)
-        keys = filter(totals.get, totals)
-        for key in sorted(keys, key=lambda k: (-totals[k], k)):
-            print('{}: {}'.format(key, totals[key]))
+        if result.get('after') is None:
+            keys = filter(totals.get, totals)
+            for key in sorted(keys, key=lambda k: (-totals[k], k)):
+                print('{}: {}'.format(key, totals[key]))
+        else:
+            count_words(subreddit, words, totals, result.get('after'))
