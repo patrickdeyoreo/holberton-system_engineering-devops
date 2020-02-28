130ea89e5a8c13bda28e7a632e2ca3735331ebe7
Patrick DeYoreo <pdeyoreo@gmail.com>
2020-02-28 01:51:38 -0800

    Shortened sed command

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -4,7 +4,7 @@ service {'nginx':
   ensure => running
 }
 
-exec {'sed -E -i \'s/^(ULIMIT=.*-n) [[:digit:]]+\>/\1 1024/\' /etc/default/nginx':
+exec {'sed -E -i \'s/^(ULIMIT=.*\<-n) [0-9]+\>/\1 1024/\' /etc/default/nginx':
   path   => '/usr/bin:/usr/sbin:/bin:/sbin',
   notify => Service['nginx']
 }
